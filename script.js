fbdf8f65ae2a14e8adcbcdd5807cf774fe555225
Qj Stout <qjstoutspykers@gmail.com>
2021-08-14 09:08:48 +1000

    Wire number buttons to display and model

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const display = new Proxy({}, databindingHandler());
+display.data = 0;
+attachEventListeners();
 
 function operate(operator, a, b) {
     const operations = {
@@ -19,7 +21,7 @@ function subtract(a, b) {
 }
 
 function multiply(a, b) {
-    return a * b;    
+    return a * b;
 }
 
 function divide(a, b) {
@@ -40,11 +42,32 @@ function databindingHandler() {
             updateDisplay(value);
         },
         get: (target, property, receiver) => { 
-            return Number(target[property]);
+            return target[property];
         }
     };
     
     function updateDisplay(value) {
         document.querySelector('#display').innerText = value;
+    }
+}
+
+function attachEventListeners() {
+    attachNumberListeners();
+}
+
+function attachNumberListeners() {
+    const numbers = document.querySelectorAll(".numbers");
+
+    numbers.forEach(number => {
+        number.addEventListener("click", numberHandler);
+    });
+
+    function numberHandler() {
+        appendNumberToDisplay(this.innerText);
+    }
+
+    function appendNumberToDisplay(number) {
+        if (display.data === '0') { display.data = number; return; }
+        display.data = display.data.concat(number);
     }
 }
